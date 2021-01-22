881807f5c12cafa70615c4ddcf5b17008884e7e2
Arthur Matta <arthurmatta@hotmail.com>
2021-01-22 17:51:31 +0000

    Added function to find error code

--- mirri/io/writers/error.py
+++ mirri/io/writers/error.py
@@ -10,6 +10,8 @@ from inspect import signature
 ERROR_LOG_FOLDER = '.\\logs'
 DOCS_FOLDER = '..\\docs' # relative path originating from ERROR_LOG_FOLDER
 
+
+
 class Entity():
         def __init__(self, acronym):
             self.entity_acronyms = [func for func in dir(self) if func.isupper() and callable(getattr(self, func)) and not func.startswith("__")]
@@ -56,18 +58,126 @@ class Entity():
 
 
 class Error():
-    def __init__(self, code, data=''):
-        """
-            Error class to store error infomration
+    # def __init__(self, code, data=''):
+    #     """
+    #         Error class to store error infomration
 
-            params
-                code: code of the error
-                data: data used by some error messages that require some data associated with the error
-        """
-        self.code = code
-        self.data = data
+    #         params
+    #             code: code of the error
+    #             data: data used by some error messages that require some data associated with the error
+    #     """
+    #     self.code = code
+    #     self.data = data
+    #     self.message = self.ErrorMessage().message(self.code, self.data)
+    #     self.entity = Entity(self.code[:3])
+
+    def __init__(self, **kwargs):
+        if 'code' in kwargs:
+            self.code = kwargs['code']
+            self.entity = Entity(self.code[:3])
+        elif 'category' in kwargs and 'entity' in kwargs and 'column' in kwargs:
+            self.category = kwargs['category']
+            self.entity = Entity(kwargs['entity'])
+            self.column = kwargs['column']
+            self.code = self.find_error_code()
+        elif len(kwargs) == 0:
+            raise TypeError('Error requires at least 1 keyword argument but none were given.')
+        else:
+            raise TypeError(f'')
+
+        self.data = kwargs['data'] if 'data' in kwargs else ''
         self.message = self.ErrorMessage().message(self.code, self.data)
-        self.entity = Entity(self.code[:3])
+
+    def find_error_code(self):
+        if self.category == 'Structure':
+            if self.column == 'Growth media': return f'{self.entity.acronym}01'
+            elif self.column == 'Geographic origin': return f'{self.entity.acronym}02'
+            elif self.column == 'Literature': return f'{self.entity.acronym}03'
+            elif self.column == 'Sexual state': return f'{self.entity.acronym}04'
+            elif self.column == 'Strains': return f'{self.entity.acronym}05'
+            elif self.column == 'Ontobiotope': return f'{self.entity.acronym}06'
+            elif self.column == 'Markers': return f'{self.entity.acronym}07'
+            elif self.column == 'Genomic information': return f'{self.entity.acronym}08'
+        elif self.category == 'Mandatory':
+            if self.entity.acronym == 'GMD':
+                if self.column == 'Acronym': return f'{self.entity.acronym}01'
+                elif self.column == 'Description': return f'{self.entity.acronym}02'
+            elif self.entity.acronym == 'GOD':
+                if self.column == 'ID': return f'{self.entity.acronym}01'
+                elif self.column == 'Country': return f'{self.entity.acronym}02'
+                elif self.column == 'Locality': return f'{self.entity.acronym}04'
+            elif self.entity.acronym == 'LID':
+                if self.column == 'ID': return f'{self.entity.acronym}01'
+                elif self.column == 'Full reference': return f'{self.entity.acronym}02'
+                elif self.column == 'Authors': return f'{self.entity.acronym}03'
+                elif self.column == 'Title': return f'{self.entity.acronym}05'
+                elif self.column == 'Journal': return f'{self.entity.acronym}07'
+                elif self.column == 'Year': return f'{self.entity.acronym}09'
+                elif self.column == 'Volume': return f'{self.entity.acronym}11'
+                elif self.column == 'First page': return f'{self.entity.acronym}13'
+            elif self.entity.acronym == 'STD':
+                if self.column == 'Accession number': return f'{self.entity.acronym}01'
+                elif self.column == 'Restriction on use': return f'{self.entity.acronym}04'
+                elif self.column == 'Nagoya protocol compliance conditions': return f'{self.entity.acronym}06'
+                elif self.column == 'Risk Group': return f'{self.entity.acronym}11'
+                elif self.column == 'Organism type': return f'{self.entity.acronym}15'
+                elif self.column == 'Taxon name': return f'{self.entity.acronym}17'
+                elif self.column == 'Recommended growth temperature': return f'{self.entity.acronym}26'
+                elif self.column == 'Recommended medium for growth': return f'{self.entity.acronym}28'
+                elif self.column == 'Form of supply': return f'{self.entity.acronym}30'
+                elif self.column == 'Geographic origin': return f'{self.entity.acronym}34'
+        elif self.category == 'Missing':
+            if self.entity.acronym == 'LID':
+                if self.column == 'Authors': return f'{self.entity.acronym}04'
+                elif self.column == 'Title': return f'{self.entity.acronym}06'
+                elif self.column == 'Journal': return f'{self.entity.acronym}08'
+                elif self.column == 'Year': return f'{self.entity.acronym}10'
+                elif self.column == 'Volume': return f'{self.entity.acronym}12'
+                elif self.column == 'First page': return f'{self.entity.acronym}14'
+            elif self.entity.acronym == 'STD':
+                if self.column == 'Taxon name': return f'{self.entity.acronym}18'
+                elif self.column == 'Recommended growth temperature': return f'{self.entity.acronym}27'
+                elif self.column == 'Recommended medium': return f'{self.entity.acronym}29'
+                elif self.column == 'Forms of supply': return f'{self.entity.acronym}31'
+                elif self.column == 'Geographic origin': return f'{self.entity.acronym}35'
+        elif self.category == 'Specification':
+            if self.entity.acronym == 'GOD':
+                if self.column == 'Country': return f'{self.entity.acronym}03'
+            elif self.entity.acronym == 'STD':
+                if self.column == 'Accession number': return f'{self.entity.acronym}02'
+                elif self.column == 'Other culture collection numbers': return f'{self.entity.acronym}03'
+                elif self.column == 'Restriction on use': return f'{self.entity.acronym}05'
+                elif self.column == 'Nagoya protocol compliance conditions': return f'{self.entity.acronym}07'
+                elif self.column == 'ABS related files': return f'{self.entity.acronym}08'
+                elif self.column == 'MTA file': return f'{self.entity.acronym}09'
+                elif self.column == 'Strain from a registered collection': return f'{self.entity.acronym}10'
+                elif self.column == 'Risk group': return f'{self.entity.acronym}12'
+                elif self.column == 'Dual use': return f'{self.entity.acronym}13'
+                elif self.column == 'Quarentine in europe': return f'{self.entity.acronym}14'
+                elif self.column == 'Organism type': return f'{self.entity.acronym}16'
+                elif self.column == 'Taxon name': return f'{self.entity.acronym}18'
+                elif self.column == 'Infrasubspecific names': return f'{self.entity.acronym}19'
+                elif self.column == 'History of deposit': return f'{self.entity.acronym}20'
+                elif self.column == 'Date of deposit': return f'{self.entity.acronym}21'
+                elif self.column == 'Date of collection': return f'{self.entity.acronym}22'
+                elif self.column == 'Date of isolation': return f'{self.entity.acronym}23'
+                elif self.column == 'Date of inclusion in the catalogue': return f'{self.entity.acronym}24'
+                elif self.column == 'Tested temperature growth range': return f'{self.entity.acronym}25'
+                elif self.column == 'Coordinates of geographic origin': return f'{self.entity.acronym}32'
+                elif self.column == 'Altitude of geographic origin': return f'{self.entity.acronym}33'
+                elif self.column == 'GMO': return f'{self.entity.acronym}36'
+                elif self.column == 'Literature': return f'{self.entity.acronym}37'
+                elif self.column == 'Sexual state': return f'{self.entity.acronym}38'
+                elif self.column == 'Ploidy': return f'{self.entity.acronym}39'
+                elif self.column == 'interspecific hybrid': return f'{self.entity.acronym}40'
+                elif self.column == 'Plasmids collections fields': return f'{self.entity.acronym}41'
+                elif self.column == 'Ontobiotope term for the isolation habitat': return f'{self.entity.acronym}42'
+                elif self.column == 'Literature linked to the sequence/genome': return f'{self.entity.acronym}43'
+            elif self.entity.acronym == 'GID':
+                if self.column == 'Strain AN': return f'{self.entity.acronym}01'
+                elif self.column == 'Marker': return f'{self.entity.acronym}02'
+                elif self.column == 'INSDC AN': return f'{self.entity.acronym}03'
+                elif self.column == 'Sequence': return f'{self.entity.acronym}04'
 
     @property
     def entity(self):
@@ -286,10 +396,10 @@ class Error():
             return f"The 'Restriction on Use' for strain with Accession Number {accession_number} is not according to the specification."
 
         def STD06(self):
-            return "The 'Nagoya Protocol restrictions and compliance conditions' is a mandatory field for each strain. The column can not be empty."
+            return "The 'Nagoya Protocol compliance conditions' is a mandatory field for each strain. The column can not be empty."
 
         def STD07(self, accession_number):
-            return f"The 'Nagoya Protocol restrictions and compliance conditions' for strain with Accession Number {accession_number} is not according to the specification."
+            return f"The 'Nagoya Protocol compliance conditions' for strain with Accession Number {accession_number} is not according to the specification."
 
         def STD08(self, accession_number):
             return f"The 'ABS related files' for strain with Accession Number {accession_number} is not a valid URL."
@@ -309,91 +419,94 @@ class Error():
         def STD13(self, accession_number):
             return f"The 'Dual Use' for strain with Accession Number {accession_number} is not according to specification."
 
-        def STD14(self):
+        def STD14(self, accession_number):
+            return f"The “Quarentine in Europe” entered for strain with Accession Number {accession_number} is incorrect."
+
+        def STD15(self):
             return "The 'Organism Type' is a mandatory field for each strain. The column can not be empty."
 
-        def STD15(self, accession_number):
+        def STD16(self, accession_number):
             return f"The 'Organism Type' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD16(self):
+        def STD17(self):
             return "The 'Taxon Name' is a mandatory field for each strain. The column can not be empty."
 
-        def STD17(self, accession_number):
+        def STD18(self, accession_number):
             return f"The 'Taxon Name' is missing for strain with Accession Number {accession_number}."
 
-        def STD18(self, accession_number):
+        def STD19(self, accession_number):
             return f"The 'Infrasubspecific Names' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD19(self, accession_number):
+        def STD20(self, accession_number):
             return f"The 'History of Deposit' sequence for strain with Accession Number {accession_number} is incorrect."
 
-        def STD20(self, accession_number):
+        def STD21(self, accession_number):
             return f"The 'Date of Deposit' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD21(self, accession_number):
+        def STD22(self, accession_number):
             return f"The 'Date of Collection' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD22(self, accession_number):
+        def STD23(self, accession_number):
             return f"The 'Date of Isolation' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD23(self, accession_number):
+        def STD24(self, accession_number):
             return f"The 'Date of Inclusion in the Catalogue' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD24(self):
+        def STD25(self):
             return f"The 'Tested Temperature Growth Range' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD25(self):
+        def STD26(self):
             return f"The 'Recommended Growth Temperature' is a mandatory field for eash strain. The column can not be empty."
 
-        def STD26(self, accession_number):
+        def STD27(self, accession_number):
             return f"The 'Recommended Growth Temperature' is missing for strain with Accession Number {accession_number}."
 
-        def STD27(self):
+        def STD28(self):
             return "The 'Recommended Medium for Growth' is a mandatory field for each strain. The column can not be empty."
 
-        def STD28(self, accession_number):
+        def STD29(self, accession_number):
             return f"The 'Recommended Medium for Growth' is missing for strain with Accession Number {accession_number}."
 
-        def STD29(self):
+        def STD30(self):
             return "The 'Forms of Supply' is a mandatory field for each Strain. The column can not be empty."
 
-        def STD30(self, accession_number):
+        def STD31(self, accession_number):
             return f"The 'Forms of Supply' is missing for strain with Accession Number {accession_number}."
 
-        def STD31(self, accession_number):
+        def STD32(self, accession_number):
             return f"The 'Coordinates of Geographic Origin' for strain with Accession Number {accession_number} are incorrect."
 
-        def STD32(self, accession_number):
+        def STD33(self, accession_number):
             return f"The 'Altitude of Geographic Origin' for strain with Accession Number {accession_number} are incorrect."
 
-        def STD33(self):
+        def STD34(self):
             return "The 'Geographic Origin' is a mandatory field for each strain. The column can not be empty."
 
-        def STD34(self, accession_number):
+        def STD35(self, accession_number):
             return f"The 'Geographic Origin' is missing or incorrect for strain with Accession Number {accession_number}."
 
-        def STD35(self, accession_number):
+        def STD36(self, accession_number):
             return f"The 'GMO' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD36(self, accession_number):
+        def STD37(self, accession_number):
             return f"The 'Literature' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD37(self, accession_number):
+        def STD38(self, accession_number):
             return f"The 'Sexual State' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD38(self, accession_number):
+        def STD39(self, accession_number):
             return f"The 'Ploidy' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD39(self, accession_number):
+        def STD40(self, accession_number):
             return f"The 'Interspecific Hybrid' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD40(self, accession_number):
+        def STD41(self, accession_number):
             return f"The 'Plasmids Collection Fields' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD41(self, accession_number):
+        def STD42(self, accession_number):
             return f"The 'Ontobiotope Term for the Isolation Habitat' for strain with Accession Number {accession_number} is incorrect."
 
-        def STD42(self, accession_number):
+        def STD43(self, accession_number):
             return f"The 'Literature Linked to the Sequence/Genome' for strain with Accession Number {accession_number} is incorrect."
 
         """
@@ -428,7 +541,6 @@ class ErrorLog():
         self.id = 0
         self.errors = {}
         self.document = docx.Document(f'.\\docs\\Error_Log_Style_Sheet.docx')
-        print([style.name for style in self.document.styles])
             
 
     def write(self, path: str):
@@ -463,7 +575,7 @@ class ErrorLog():
         subject = 'Validator Error Log'
 
         self.document.add_paragraph(f'Dear Curator{cc},')
-        paragraph = self.document.add_paragraph(f'the Excel File {self.input_filename} you\'ve provided {date}with your collection Strains Data contains errors/missing data.\n')
+        paragraph = self.document.add_paragraph(f'the Excel File {self.input_filename} you\'ve provided {date}with your collection Strains Data contains errors/missing data. ')
         paragraph.add_run('Please, see below the list of detected errors/missing data, for you to proceed with the appropriated correction/completion.')
 
         paragraph = self.document.add_paragraph('If you need help, please refer to the instructions contained in "')
@@ -475,7 +587,7 @@ class ErrorLog():
 
         self.document.add_page_break()
         
-        self.document.add_heading(Entity('EFS').name, level=1).style = self.document.styles['Heading 1']
+        self.document.add_heading(f'Analysis of {Entity("EFS").name}', level=1).style = self.document.styles['Heading 1']
         self.document.add_paragraph('The structure of your Excel File show the following changes, as compared to the original Template:')
 
         table = self.document.add_table(rows=2, cols=2)
@@ -655,18 +767,28 @@ class ErrorLog():
 
 
 if __name__ == '__main__':
-    error_log = ErrorLog('MIRRI-IS_dataset_BEA_template_30092020', 'BEA')
+    error_log = ErrorLog('MIRRI-IS_dataset_BEA_template_30092020', 'BEA', '30-09-2020')
+    # errors = [
+    #     Error('EFS01'),
+    #     Error('EFS02'),
+    #     Error('GOD01'),
+    #     Error('GOD04', 'Brazil'),
+    #     Error('LID01'),
+    #     Error('STD02'),
+    #     Error('STD07', 'AN 123'),
+    #     Error('STD16'),
+    #     Error('STD23', 'AN 123'),
+    #     Error('GID01', 'AN 456'),
+    #     Error('GID03', 'AN 456'),
+    # ]
     errors = [
-        Error('EFS01'),
-        Error('EFS02'),
-        Error('STD01'),
-        Error('GOD01'),
-        Error('LID01')
+        Error(category='Missing', entity='STD', column='Taxon name', data='AN 123'),
+        Error(category='Mandatory', entity='GMD', column='Acronym'),
+        Error(category='Specification', entity='GOD', column='Country', data='Brazil')
     ]
-
     for error in errors:
         error_log.add_error(error)
 
     print(error_log)
-    print(f'Writing to file \'{ERROR_LOG_FOLDER}\Error_Log_Example.docx\'')
-    error_log.write(ERROR_LOG_FOLDER)
+    # print(f'Writing to file \'{ERROR_LOG_FOLDER}\Error_Log_Example.docx\'')
+    # error_log.write(ERROR_LOG_FOLDER)
