675c3d7dd35ab3fd2884070fdd9c178fe7fe7a5f
Arthur Matta <arthurmatta@hotmail.com>
2021-05-20 14:14:36 +0100

    improvements to error log readability

--- mirri/validation/error_logging/error_log.py
+++ mirri/validation/error_logging/error_log.py
@@ -1,31 +1,28 @@
-from typing import Optional
+from typing import Optional, Union
 from datetime import datetime
 from .error import Error
 
 
 class ErrorLog():
-    """
-        Error Logging
+    def __init__(self, input_filename: str, cc: Optional[str] = None, date: Optional[Union[str, datetime]] = None, limit: int = 100):
+        """
+        Logger for Error instances.
 
         Args:
-            input_filename: name of the file which the error log is derived from
-            cc [optional]: culture collection identifier
-            date [optional]: date the inputed file was submited for validation (date of last modification)
-            limit [int]: maximum number fo errors to be writen on the output file
-    """
-
-    def __init__(self, input_filename: str, cc: str = None, date: str = None, limit: int = 100):
-        self.input_filename = input_filename
-        self.cc = cc
-        self.date = datetime.strptime(
-            date, r'%d-%m-%Y').date() if date is not None else None
-
+            input_filename (str): name of the file to be logged
+            cc (str, optional): name of the curator. Defaults to None.
+            date (str, optional): date (e.g. created, last modified) associated with the file. Useful for versioning. Defaults to None.
+            limit (int, optional): limit of errors to print to the report. Defaults to 100.
+        """
+        self._input_filename = input_filename
+        self._cc = cc
+        self._date = date
         self._errors = {}
         self.limit = limit
         self._counter = 0
 
     def __str__(self) -> str:
-        output = f"""Error Log for file {self.input_filename}\nENTITY | CODE   | MESSAGE"""
+        output = f"""Error Log for file {self._input_filename}\nENTITY | CODE   | MESSAGE"""
         for acronym, error_list in self.get_errors().items():
             for error in error_list:
                 output += f"\n{acronym:6} | {error.code:6} | {error.message[:100]}"
@@ -40,25 +37,40 @@ class ErrorLog():
         self._input_filename = input_filename
 
     @property
-    def cc(self) -> str:
+    def cc(self) -> Optional[str]:
         return self._cc
 
     @cc.setter
-    def cc(self, cc: str) -> None:
+    def cc(self, cc: Optional[str]) -> None:
         self._cc = cc
 
     @property
-    def date(self) -> Optional[datetime]:
+    def date(self) -> Optional[Union[str, datetime]]:
         return self._date
 
     @date.setter
-    def date(self, date: Optional[datetime] = None) -> None:
-        self._date = date
+    def date(self, date: Optional[Union[str, datetime]] = None) -> None:
+        if isinstance(date, str):
+            self._date = datetime.strptime(date, r'%d-%m-%Y')
+        else:
+            self._date = date
 
     def get_errors(self) -> dict:
+        """
+        Get all errors
+
+        Returns:
+            dict: Error intances grouped by entity acronym.
+        """
         return self._errors
 
-    def add_error(self, error: Error) -> list:
+    def add_error(self, error: Error) -> None:
+        """
+        Add an error.
+
+        Args:
+            error (Error): Error instance.
+        """
         if error.entity.acronym not in self._errors:
             self._errors[error.entity.acronym] = [error]
         else:
